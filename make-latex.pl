e5f971e2bb0679de200ea95b90926cf67eb72b4c
CaptainLebyadkin <CaptainLebyadkin@users.noreply.github.com>
2015-05-18 13:22:48 +0930

    Update make-latex.pl

--- make-latex.pl
+++ make-latex.pl
@@ -23,7 +23,7 @@ use IO::File;
 use Getopt::Long;
 use IO::Socket;
 
-my $tmp_dir = "~/ztmp.$$";
+my $tmp_dir = "/tmp/ztmp.$$";
 my $tmp_pre = "$tmp_dir/zmakelatex.";
 my $tex_prefix = "zmakelatex.$$";
 my $tex_file = "$tmp_pre$$.tex";
